72384cb841997d47671eb4edf8a6f81bb68eed1d
silomaben <kaybernard449@gmail.com>
2023-08-01 20:25:36 +0300

    function rich django blog

---
+++ blog_cee/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from . import views
+
+from .views import Homeview,ArticleDetailView,Categoryview,CategoryListView,AddPostView,UpdatePostView,DeletePostView,AddCategoryView,LikeView,AddCommentView
+
+
+# urlpatterns = [
+#     path('',views.home,name='home'),
+#     path('blogpost',views.blogpost,name='blogpost'),
+# ]
+
+urlpatterns = [
+    path('',Homeview.as_view(), name='home'),
+    path('article/<int:pk>',ArticleDetailView.as_view(), name='article-detail'),
+    path('add_post', AddPostView.as_view(), name='add_post'),
+    path('add_category', AddCategoryView.as_view(), name='add_category'),
+    path('article/edit/<int:pk>',UpdatePostView.as_view(), name='update_post'),
+    path('article/<int:pk>/remove',DeletePostView.as_view(), name='delete_post'),
+    path('category/<str:cats>', Categoryview, name='category'),
+    path('category_list', CategoryListView, name='category_list'),
+    path('like/<int:pk>', LikeView, name='like_post'),
+    path('article/<int:pk>/add_comment/', AddCommentView.as_view(), name='add_comment'),
+]
+
+# remember to slugify categories links
